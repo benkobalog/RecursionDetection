7a22ceb9cff08b3bc85aa9a026db840e5bd2551d
benkobalog <benko.balog@gmail.com>
2017-12-18 00:23:09 +0100

    This can find abab, abcabc and so on type of recursion as well. One problem is that if there's multiple recursive parts it puts both in the same list, despite they two parts are different

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,12 +1,17 @@
 object Main extends App {
-  val res = RecursionDetection.compactRepeats(List(1, 2, 4, 4, 4, 5, 1, 23, 1, 1, 1, 2))
+  val l1 = List(1, 2, 4, 4, 4, 5, 1, 23, 1, 1, 1, 2)
+  val l = List(1, 2, 3, 2, 3, 2, 2)
+  val res = RecursionDetection.repeatedParts(l)
 
-  res.foreach(println)
+  val l3 = List(5, 5, 5, 5, 4, 1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 5)
+  val res2 = RecursionDetection.repeatedParts(l3)
 
-  val res2 = RecursionDetection.indexMap(res)
-
+  println(res)
   println(res2)
 
-  println(RecursionDetection.listFromIndexMap(res2))
+  val charList = "aaaabcccccc".toList
+
+  println(RecursionDetection.repeatedParts(charList))
+
 
 }
--- src/main/scala/RecursionDetection.scala
+++ src/main/scala/RecursionDetection.scala
@@ -1,45 +1,24 @@
-import scala.annotation.tailrec
-
 object RecursionDetection {
-  case class RepeatableElement[A](a: A, times: Int) {
-    require(times > 0)
-  }
 
-  type CompactList[A] = List[RepeatableElement[A]]
+  def repeatedParts[A](list: List[A]) = {
+    val maxPossibleRepetitionLength = list.length / 2
 
-  def compactRepeats[A](list: List[A]): CompactList[A] = {
-    @tailrec
-    def loop(remainder: List[A], acc: CompactList[A]): CompactList[A] =
-      remainder match {
-        case Nil => acc
-        case x :: xs =>
-          if (acc.nonEmpty && x == acc.head.a)
-            loop(xs, RepeatableElement(x, acc.head.times + 1) :: acc.tail)
-          else
-            loop(xs, RepeatableElement(x, 1) :: acc)
+    (1 to maxPossibleRepetitionLength)
+      .map { repetitionLength =>
+        list.zipWithIndex
+          .sliding(repetitionLength * 2)
+          .filter { x =>
+            val (left, right) = x.splitAt(repetitionLength)
+            left.map(_._1) == right.map(_._1)
+          }
+          .flatten
+          .toSet
+          .toList
+          .sortWith(_._2 < _._2)
+          .map(_._1)
       }
-    loop(list, Nil).reverse
-  }
-
-  case class IndexAndTimes(idx: Int, times: Int)
-
-  def indexMap[A](xs: CompactList[A]): Map[A, List[IndexAndTimes]] =
-    // The indexes are needed so the list can be rebuilt from produced the Map
-    xs.zipWithIndex
-      .groupBy(_._1.a)
-      .mapValues(_.map(x => IndexAndTimes(x._2, x._1.times)))
-
-  def listFromIndexMap[A](idxMap: Map[A, List[IndexAndTimes]],
-                          fromIdx: Int = 0): List[A] = {
-    idxMap.find(x => x._2.exists(_.idx == fromIdx)) match {
-      case Some(x) =>
-        val times = x._2.find(_.idx == fromIdx).get.times
-        val updatedList = x._2.filterNot(_ == IndexAndTimes(fromIdx, times))
-        val updatedMap = idxMap + (x._1 -> updatedList)
-        List.fill(times)(x._1) ::: listFromIndexMap(updatedMap, fromIdx + 1)
-      case None =>
-        List[A]()
-    }
+      .filterNot(_.length == 0)
+      .distinct
   }
 
 }
